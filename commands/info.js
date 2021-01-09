bc956b287744e60632989dc8d4015711013e90fc
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2021-01-09 14:19:07 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -3,13 +3,13 @@ const discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
 
         var botEmbed = new discord.MessageEmbed()
-			.setTitle("Dough Informatie")
+			.setTitle("Arnhem Informatie")
 			.addField("Versie", "`V1.0.0`")
             .addField("Eigenaar", "`SyntaxErroR#0001`")
 			.addField("Gebruikers", `${client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)}`)
 			.addField("Donatie", "[Robux Only](https://www.roblox.com/game-pass/13929030/Donatie-Arnhem)") 
 			.setColor("#2075d6")
-			.setFooter(`Aangevraagd door: ${message.autor}`)
+			.setFooter(`Aangevraagd door: ${message.author.username}`)
 			.setThumbnail(`${message.guild.iconURL()}`)
 
         return message.channel.send(botEmbed);
